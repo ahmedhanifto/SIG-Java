b20052faf1016c730442ab70c8ecb2002fb23297
ahmedhanifto <118386164+ahmedhanifto@users.noreply.github.com>
2022-12-26 22:53:33 +0200

    Delete SIG-Java directory

--- SIG-Java/src/Main.java
+++
@@ -1,9 +0,0 @@
-import view.Layout;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Layout l1 =new Layout();
-        l1.setVisible(true);
-    }
-}
